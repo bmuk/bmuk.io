33033ccb100a7996979b64c07f98c18440e1c71b
Britt Mathis <bmathis@protectedtrust.com>
2015-07-14 04:03:36 -0400

    Hello Suave

--- tests/BmukIO.Tests/Tests.fs
+++ tests/BmukIO.Tests/Tests.fs
@@ -4,7 +4,7 @@ open BmukIO
 open NUnit.Framework
 
 [<Test>]
-let ``hello returns 42`` () =
-  let result = Library.hello 42
+let ``Dummy Test`` () =
+  let result = 42
   printfn "%i" result
   Assert.AreEqual(42,result)
