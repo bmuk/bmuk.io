0372060df42dd94d19f00470e749ed6380779af6
Britt Mathis <bmathis@protectedtrust.com>
2015-07-14 04:14:03 -0400

    Look Ma, no main!

--- src/BmukIO/App.fs
+++ src/BmukIO/App.fs
@@ -4,7 +4,4 @@ open Suave
 open Suave.Http.Successful
 open Suave.Web
 
-[<EntryPoint>]
-let main argv =
-  startWebServer defaultConfig (OK "Hello World!")
-  0
+startWebServer defaultConfig (OK "Hello World!")
